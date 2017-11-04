8c0d832f48fac1b8b867cf7a9950a63b0e0b01f1
Cleepople <33344447+Cleepople@users.noreply.github.com>
2017-11-04 02:32:02 -0700

    Add files via upload

---
+++ WeatherinCity.py
@@ -0,0 +1,47 @@
+"""
+Written by: Zaid Ahmad
+e-mail: zaid.ahmad211@gmail.comm
+"""
+
+
+import re
+import json
+import urllib
+
+
+def get_ip():
+    url = "http://checkip.dyndns.org"
+    data = urllib.urlopen(url).read()
+    ip_address = data[-30:-16]
+    return ip_address
+
+
+def get_weather(ip_address):
+    end_point = "http://api.worldweatheronline.com/free/v1/weather.ashx?" 
+    query = "key=a37fae643df77aa83d88abbc9e8e96194ab242d4&q=" + str(ip_address) + "&num_of_days=0&format=json"
+    url = end_point +  query
+    json_data = urllib.urlopen(url).read()
+    data = json.loads(json_data)
+    current_weather = data['data']['current_condition'][0]
+    return current_weather
+
+
+def print_weather(data):
+    print """
+    Weather : %s 
+    Temperatue : %s Celsius
+    Wind : %s Kmph %s 
+    Humidity : %s 
+    Precipitation : %s MM
+    """ % (data['weatherDesc'][0]['value'], data['temp_C'], data['windspeedKmph'], data['winddir16Point'], data['humidity'], data['precipMM'])
+
+
+def main():
+    print "\nGetting weather information for your location..."
+    ip_address = get_ip()
+    data = get_weather(ip_address)
+    print_weather(data)
+
+
+if __name__ == "__main__":
+    main()
